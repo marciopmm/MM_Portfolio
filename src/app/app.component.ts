d8f828bc90e53895b9c55bd1963bed11d6d95f60
Marcio Martins <marciopmm@gmail.com>
2023-02-03 15:54:43 -0300

    Last coomit before Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'MM_Portfolio';
+export class AppComponent implements OnInit {
+
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      name: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      message: [null, Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formGroup);
+    if (!this.formGroup.valid) {
+      Swal.fire('Pay attention, please', 'There are validations errors', 'warning');
+    }
+  }
+
+  applyCssError(field: string) {
+    let fieldControl = this.formGroup.get(field);
+    return {
+      '': !fieldControl?.touched,
+      'is-valid': fieldControl?.valid && fieldControl?.touched,
+      'is-invalid': !fieldControl?.valid && fieldControl?.touched
+    };
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyABAn8Qw-qlGohF5ynOZKN5sXkDvlzQpCU",
+        authDomain: "mm-portfolio-3c551.firebaseapp.com",
+        projectId: "mm-portfolio-3c551",
+        storageBucket: "mm-portfolio-3c551.appspot.com",
+        messagingSenderId: "393369565463",
+        appId: "1:393369565463:web:f72cf2be52534d6c485f52"
+    }
+};
