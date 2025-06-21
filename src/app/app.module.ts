6628dfff6662cb290ab37252e94379b75c240566
Marcio Martins <marciopmm@gmail.com>
2025-06-21 17:57:15 +0100

    Update to renew experiences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,22 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
 
 import { environment } from '../environments/environment.development';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkComponent } from './components/work/work.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EmailComponent } from './components/email/email.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    WorkComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
